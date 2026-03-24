b262f0f8574c6e422e9d75501e58ef730378373d
gustavo-fumagalli <g.fumagalli006@gmail.com>
2026-03-24 12:19:32 -0300

    model criado

---
+++ src/test/java/com/fumagalli/gerenciamento_clientes/GerenciamentoClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fumagalli.gerenciamento_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
